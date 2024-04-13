e421fa80a97b3d0859a00e0b07bda9f730cf8ae4
devang30github <gawadedevang@gmail.com>
2024-04-13 23:20:51 +0530

    styling improve still  remaining

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,13 +1,25 @@
+# forms.py
+
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
-class UserRegistrationForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['username', 'password1', 'password2']
-
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(label="Username", max_length=30,
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
+    password = forms.CharField(label="Password", max_length=30,
+                               widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+
+class UserRegistrationForm(UserCreationForm):
+    username = forms.CharField(label="Username", max_length=30,
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
+    email = forms.EmailField(label="Email", max_length=100,
+                             widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
+    password1 = forms.CharField(label="Password", max_length=30,
+                                widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+    password2 = forms.CharField(label="Confirm Password", max_length=30,
+                                widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'}))
+
     class Meta:
         model = User
-        fields = ['username', 'password']
+        fields = ['username', 'email', 'password1', 'password2']
